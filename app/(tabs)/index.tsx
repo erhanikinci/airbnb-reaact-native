de5ea6118ef9130519fac8967b0af1e15c50601d
Erhan İkinci <erhanikinci61@gmail.com>
2024-01-16 00:58:28 +0300

    V-1.0.1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,62 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import React, { useMemo, useState } from "react";
+import { Stack } from "expo-router";
+import ExploreHeader from "@/components/ExploreHeader";
+import Listings from "@/components/Listings";
+import listingsData from "@/assets/data/airbnb-listings.json";
+import listingsDataGeo from "@/assets/data/airbnb-listings.geo.json";
+import ListingsMap from "@/components/ListingsMap";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import ListingsBottomSheet from "@/components/ListingsBottomSheet";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+const GOOGLEAPI_API_KEY = process.env.EXPO_PUBLIC_GOOGLEAPI_API_KEY;
 
-export default function TabOneScreen() {
+const Page = () => {
+  const [category, setCategory] = useState("Tiny homes");
+  const items = useMemo(() => listingsData as any, []);
+  const geoItems = useMemo(() => listingsDataGeo as any, []);
+
+  const onDataChanged = (category: string) => {
+    console.log("CHANGED_", category);
+
+    setCategory(category);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={{ flex: 1, marginTop: 130 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+
+      {/* <Listings listings={items} category={category} /> */}
+      <ListingsMap listings={geoItems} />
+      <ListingsBottomSheet listings={items} category={category} />
+      
+
+      
     </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  searchContainer: {
+    position: 'absolute',
+    width: "90%",
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 4,
+    padding: 8,
+    borderRadius: 8,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+  input: {
+    borderColor: "#888",
+    borderWidth: 1
+  }
+})
+export default Page;
